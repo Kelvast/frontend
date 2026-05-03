edabaa0f286c3399aa40b1cb6c4dc28968c96b8a
SamNewhouse <hello@samnewhouse.co.uk>
2026-05-03 23:03:34 +0100

    more chunks

--- src/game-client/world/regions/spawn/index.ts
+++ src/game-client/world/regions/spawn/index.ts
@@ -6,7 +6,12 @@ const region: Region = {
   id: "spawn",
   name: "spawn",
   chunks: {
+    "-1,0": { ...chunk_-1_0, chunkX: 0, chunkZ: 0, region: "spawn", objects: [], npcSpawns: [] },
     "0,0": { ...chunk_0_0, chunkX: 0, chunkZ: 0, region: "spawn", objects: [], npcSpawns: [] },
+    "0,1": { ...chunk_0_0, chunkX: 0, chunkZ: 0, region: "spawn", objects: [], npcSpawns: [] },
+    "0,2": { ...chunk_0_0, chunkX: 0, chunkZ: 0, region: "spawn", objects: [], npcSpawns: [] },
+    "1,0": { ...chunk_0_0, chunkX: 0, chunkZ: 0, region: "spawn", objects: [], npcSpawns: [] },
+    "2,0": { ...chunk_0_0, chunkX: 0, chunkZ: 0, region: "spawn", objects: [], npcSpawns: [] },
   },
 };
 
