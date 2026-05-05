de75c1cbcc77bdb3c2b860766d1f912bfccfe7b4
SamNewhouse <hello@samnewhouse.co.uk>
2026-05-05 02:50:56 +0100

    Add wordmark to nav

--- src/presentation/3-organisms/LandingNav.tsx
+++ src/presentation/3-organisms/LandingNav.tsx
@@ -1,11 +1,10 @@
 import { FC, memo } from "react";
+import KelvastWordmark from "../1-atoms/KelvastWordmark";
 
 const LandingNav: FC = () => {
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 flex items-center px-6 py-5 md:px-12">
-      <span className="font-heading font-black text-xl tracking-tight text-[var(--color-text)]">
-        Kelvast
-      </span>
+      <KelvastWordmark className="h-6 md:h-8"/>
     </nav>
   );
 };
