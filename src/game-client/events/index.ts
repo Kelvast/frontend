86f0fdb96c3d48141615f3eb97f61acf9cc6c6ce
SamNewhouse <hello@samnewhouse.co.uk>
2026-05-10 20:18:34 +0100

    Removed barrel for helpers

--- src/game-client/events/index.ts
+++ src/game-client/events/index.ts
@@ -1,6 +1,5 @@
 export { gameEventBus } from "./bus";
 export { GAME_EVENT } from "./game-event";
-export { createEmitter, createListener } from "./helpers";
 export * from "./emitters";
 export * from "./listeners";
 export type { GameEventMap, GameEventKey, GameEventPayload } from "./types";
